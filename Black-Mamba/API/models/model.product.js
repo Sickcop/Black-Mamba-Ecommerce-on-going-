4795e700a76985a758e3071345b6d0d0873afd2f
Sickcop <juan,hurtado2@ucp.edu.co>
2024-08-08 23:01:15 -0500

    error

--- Black-Mamba/API/models/model.product.js
+++ Black-Mamba/API/models/model.product.js
@@ -37,7 +37,7 @@ export class ProductModel {
       return rows[0];
     } catch (error) {
       console.error('Error fetching product:', error);
-      return { message: 'not found' };
+      return { message: 'not found' a};
     }
   } 
 
